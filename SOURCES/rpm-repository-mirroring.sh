0b27c48d0e88fe57de549a905dfb1e58cc50839a
Anton Patsev <patsev.anton@gmail.com>
2018-01-01 14:57:56 +0000

    update SOURCES/mrepo.conf  SOURCES/rpm-repository-mirroring.conf  SOURCES/rpm-repository-mirroring.sh  SPECS/rpm-repository-mirroring.spec

--- SOURCES/rpm-repository-mirroring.sh
+++ SOURCES/rpm-repository-mirroring.sh
@@ -2,21 +2,12 @@
 
 # based on: http://blog.kagesenshi.org/2007/06/fedora-repository-mirroring-script.html
 
-# mirror root - the place you want the rpms to be downloaded
-#MROOT="/opt/storage/repository"
-
-# processor architectures (space separated)
-#ARCHS="x86_64"
-
-# repository names (space separated)
-#REPOS="epel nginx rhel-x86_64-server-6 rhel-x86_64-server-ha-6 rhel-x86_64-server-optional-6 rhel-x86_64-server-rs-6 vmware-tools newrelic"
-
 source /etc/rpm-repository-mirroring.conf
 
 downrepo () {
     cd $MROOT
     echo "Sychronizing Repositories"
-    reposync --download-metadata --gpgcheck --plugins --repoid=$1 --arch=$2 --newest-only -t /var/tmp/reposync-cache  
+    reposync --download-metadata --gpgcheck --plugins --repoid=$1 --arch=$2 --newest-only -t /var/tmp/reposync-cache
     STAT=$?
     if [ "$STAT" == "0" ];then
         cd $1
--- SPECS/rpm-repository-mirroring.spec
+++ SPECS/rpm-repository-mirroring.spec
@@ -2,21 +2,30 @@
 
 Name:    rpm-repository-mirroring
 Version: 0.1
-#Release: 1%{?dist}
-Release: 1
+Release: 1%{?dist}
+#Release: 1
 Summary: RPM repository mirroring script using reposync
 Group:   Development Tools
 License: ASL 2.0
 Source0: rpm-repository-mirroring.sh
+Source1: mrepo.conf
+Source2: rpm-repository-mirroring.conf
 Requires: nginx
+Requires: createrepo
 
 %description
 
 %install
 rm -rf $RPM_BUILD_ROOT
-ls -1
 mkdir -p %{buildroot}/%{_bindir}
 %{__install} -m 755 %{SOURCE0} %{buildroot}/%{_bindir}/%{name}
+mkdir -p %{buildroot}/etc/nginx/conf.d/
+cp -a %{SOURCE1} %{buildroot}/etc/nginx/conf.d/
+cp -a %{SOURCE2} %{buildroot}/etc/
+mkdir -p %{buildroot}/var/www/repos
 
 %files
 %{_bindir}/%{name}
+/etc/nginx/conf.d/mrepo.conf
+/etc/rpm-repository-mirroring.conf
+%dir /var/www/repos
