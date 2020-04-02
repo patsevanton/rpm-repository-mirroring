34939b5edcc7d2755ac95d773576fca34ea35c30
Anton Patsev <10828883+patsevanton@users.noreply.github.com>
2020-04-02 15:32:52 +0600

    Update rpm-repository-mirroring.sh

--- SOURCES/rpm-repository-mirroring.sh
+++ SOURCES/rpm-repository-mirroring.sh
@@ -9,7 +9,7 @@ source /etc/rpm-repository-mirroring.conf
 downrepo () {
     cd $MROOT
     echo "Sychronizing Repositories"
-    reposync --download-metadata --plugins --repoid=$1 --arch=$2 --allow-path-traversal
+    reposync --download-metadata --tempcache --plugins --repoid=$1 --arch=$2 --allow-path-traversal
     STAT=$?
     echo $1
     if [[ $1 == *"kubernetes"* ]]; then
