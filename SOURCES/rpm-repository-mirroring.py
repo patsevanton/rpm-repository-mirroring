c186548fc75aa3df4ef6f0d5ccac1072dc9e92bc
Anton Patsev <patsev.anton@gmail.com>
2020-05-19 14:14:13 +0600

    rpm-repository-mirroring.py

--- SOURCES/rpm-repository-mirroring.py
+++ SOURCES/rpm-repository-mirroring.py
@@ -5,7 +5,7 @@ from pprint import pprint
 
 config = "rpm-repository-mirroring.conf"
 begin_version_package_at_repo = {'kubernetes':'1.15.12-0','grafana':'5.4.5-1'}
-last_version_specific_package = {'rkt':5,'kubernetes-cni':5,'cri-tools':5}
+last_count_version_specific_package = {'rkt':5,'kubernetes-cni':5,'cri-tools':5}
 
 def get_list_repo(config):
 	with open(config) as f:
@@ -21,8 +21,8 @@ def command_yum_available_package_in_repo(repo):
 def command_yum_available_version_for_package(package):
 	return "yum -q --showduplicates list available {0}".format(package)
 
-def get_dict_uniq_package_in_repo():
-	dict_uniq_package_in_all_repo = {}
+def get_dict_uniq_packages_in_repo():
+	dict_uniq_packages_in_repo = {}
 	for repo in list_repo:
 		list_uniq_packages_custom_repo = []
 		process = subprocess.Popen(command_yum_available_package_in_repo(repo), shell=True, stdout=subprocess.PIPE)
@@ -33,15 +33,17 @@ def get_dict_uniq_package_in_repo():
 					package = line.strip().split()[0].split('.')[0]
 					if package not in list_uniq_packages_custom_repo:
 						list_uniq_packages_custom_repo.append(package)
-		if repo not in dict_uniq_package_in_all_repo:
-			dict_uniq_package_in_all_repo[repo] = list_uniq_packages_custom_repo
-	return dict_uniq_package_in_all_repo
+		if repo not in dict_uniq_packages_in_repo:
+			dict_uniq_packages_in_repo[repo] = list_uniq_packages_custom_repo
+	return dict_uniq_packages_in_repo
 
-# def get_dict_version_for_package(package, begin_version_package_at_repo,last_version_specific_package):
-def get_dict_version_for_package(package):
-	for uniq_package_in_repo in dict_uniq_package_in_all_repo:
-		if package in uniq_package_in_repo:
-			print(package,uniq_package_in_repo)
+# def get_dict_version_for_package(package, begin_version_package_at_repo,last_count_version_specific_package):
+def dict_version_of_package_uniq_packages_in_repo(dict_uniq_packages_in_repo):
+	for repo, list_uniq_packages in dict_uniq_packages_in_repo.iteritems():
+		for uniq_package in list_uniq_packages:
+			pprint(uniq_package)
+			if uniq_package in last_count_version_specific_package.iteritems():
+				pprint(uniq_package)
 
 	# process = subprocess.Popen(command_yum_available_version_for_package(package), shell=True, stdout=subprocess.PIPE)
 	# output, error = process.communicate()
@@ -49,7 +51,7 @@ def get_dict_version_for_package(package):
 	# 	print(line)
 
 # pprint(list_repo)
-# pprint(dict_uniq_package_in_all_repo)
+# pprint(dict_uniq_packages_in_repo)
 # get_dict_version_for_package('kubelet')
 
 # def main(config):
@@ -58,5 +60,7 @@ def get_dict_version_for_package(package):
 # 		get_last_version_for_package(package, begin_version_package_at_repo)
 
 list_repo = get_list_repo(config)
-dict_uniq_package_in_all_repo = get_dict_uniq_package_in_repo()
-print(dict_uniq_package_in_all_repo)
+dict_uniq_packages_in_repo = get_dict_uniq_packages_in_repo()
+# print(dict_uniq_packages_in_repo)
+# dict_version_of_package_uniq_packages_in_repo = 
+# print(dict_version_of_package_uniq_packages_in_repo(dict_uniq_packages_in_repo))
