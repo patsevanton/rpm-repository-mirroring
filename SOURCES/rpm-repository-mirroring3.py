dddaedf1881e413852517e0bb0c33414dcefa4c2
Anton Patsev <10828883+patsevanton@users.noreply.github.com>
2020-05-29 20:01:04 +0600

    Update rpm-repository-mirroring3.py

--- SOURCES/rpm-repository-mirroring3.py
+++ SOURCES/rpm-repository-mirroring3.py
@@ -1,30 +1,21 @@
 #!/usr/bin/env python
 
 import json
+import os
+import shutil
 import sys
 import yum
 from distutils.version import LooseVersion
-from pprint import pprint
 
 config = "rpm-repository-mirroring.conf"
 
-
-def get_dir_to_download(config):
-	with open(config) as f:
-		for line in f:
-			if "DESTDIR" in line:
-				fields = line.strip().split("=")
-				dir_to_download=fields[1][1:-1].split()
-				return dir_to_download
-
-
-def get_list_repo(config):
+def get_dict_repo(config):
 	with open(config, "r") as f:
 		for line in f:
 			if "REPOS" in line:
 				fields = line.strip().split("=")
-				list_repo=json.loads(fields[1])
-				return list_repo
+				dict_repo=json.loads(fields[1])
+				return dict_repo
 
 def get_dict_cut(config):
 	with open(config, "r") as f:
@@ -34,17 +25,36 @@ def get_dict_cut(config):
 				name_count=json.loads(fields[1])
 				return name_count
 
+def get_download_dir(config):
+	with open(config) as f:
+		for line in f:
+			if "DOWNLOAD_DIR" in line:
+				fields = line.strip().split("=")
+				dir=fields[1]
+				return dir
+
+def save_po(pkg):
+	remote = pkg.returnSimple('relativepath')
+	local = os.path.basename(remote)
+	local = os.path.join(download_dir, local)
+	pkg.localpath = local
+	path = pkg.repo.getPackage(pkg, copy_local=1, cache = False)
+	if not os.path.exists(local) or not os.path.samefile(path, local):
+		shutil.copy2(path, local)	
 
 name_count = get_dict_cut(config)
 repo_name_ver = {}
-for repo in get_list_repo(config):
+repo_ver = get_dict_repo(config)
+download_dir = get_download_dir(config)
+
+for repo in repo_ver:
 	yb = yum.YumBase()
 	if not yb.setCacheDir(force=True, reuse=False):
 		print >>sys.stderr, "Can't create a tmp. cachedir. "
 		sys.exit(1)
-
 	yb.repos.disableRepo('*')
 	yb.repos.enableRepo(repo)
+
 	repo_name_ver[repo] = {}
 	ignore = False
 	prev_name = None
@@ -53,23 +63,26 @@ for repo in get_list_repo(config):
 			continue
 		if prev_name != pkg.name:
 			ignore = False
+
+		pkg.repo.copy_local = True
+		pkg.repo.cache = 0
+
 		if pkg.name in name_count:
 			if name_count[pkg.name] > 0:
-				if pkg.name in repo_name_ver[repo]:
-					repo_name_ver[repo][pkg.name].append(pkg.version + '-' + pkg.release)
-				else:
-					repo_name_ver[repo][pkg.name] = [pkg.version +  '-' + pkg.release]
+				if pkg.name not in repo_name_ver[repo]:
+					repo_name_ver[repo][pkg.name] = []
+				repo_name_ver[repo][pkg.name].append(pkg.version + '-' + pkg.release)
 				name_count[pkg.name] -= 1
+				save_po(pkg)
 			else:
 				name_count.pop(pkg.name, None)
 				ignore = True
 		else:
-			if pkg.name in repo_name_ver[repo]:
-				if LooseVersion(pkg.version) >= LooseVersion(get_list_repo(config)[repo]):
-					repo_name_ver[repo][pkg.name].append(pkg.version + '-' + pkg.release)
-			else:
-				if LooseVersion(pkg.version) >= LooseVersion(get_list_repo(config)[repo]):
-					repo_name_ver[repo][pkg.name] = [pkg.version + '-' + pkg.release]
+			if LooseVersion(pkg.version) >= LooseVersion(repo_ver[repo]):
+				if pkg.name not in repo_name_ver[repo]:
+					repo_name_ver[repo][pkg.name] = []
+				repo_name_ver[repo][pkg.name].append(pkg.version + '-' + pkg.release)
+				save_po(pkg)
 		prev_name = pkg.name
 
 
@@ -81,19 +94,5 @@ def pretty(d, indent=0):
       else:
          print('\t' * (indent+1) + str(value))
 
-def get_dict_of_list_package_for_download(dict):
-	dict_of_list_package_for_download = {}
-	for repo, int_dict in dict.items():
-		list_package_for_download = []
-		for package, list_version in int_dict.items():
-			for version in list_version:
-				list_package_for_download.append(package + '-' +  version)
-		dict_of_list_package_for_download[repo] = list_package_for_download
-	return dict_of_list_package_for_download
-
-
-#pretty(repo_name_ver)
-
-
+pretty(repo_name_ver)
 #print repo_name_ver
-pprint(get_dict_of_list_package_for_download(repo_name_ver))
